65c25e1cd4ee18ea56bd1f4f929d4eddc653d854
Agnes <almoha@gmail.com>
2022-03-19 08:36:59 +0100

    rangement

--- modal.js
+++ modal.js
@@ -10,239 +10,302 @@ toggleButton.addEventListener("click", () => {
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
+const modalbg2 = document.querySelector(".bground-2");
 const modal = document.querySelector(".modal-body");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const btnSignUp = document.querySelectorAll(".btn-signup");
 const btnSubmit = document.querySelector(".btn-submit");
+const btnCloseModal = document.querySelector('.btn-closeModal');
 const form = document.querySelector("form");
-const closeModal = document.querySelector(".close");
-const prenom = document.querySelector("#first");
-const prenomHelp = document.querySelector(".prenomHelp");
-const nom = document.querySelector("#last");
-const nomHelp = document.querySelector(".nomHelp");
-const mail = document.querySelector("#email");
-const mailHelp = document.querySelector(".mailHelp");
-const birth = document.querySelector("#birthdate");
-const birthHelp = document.querySelector(".birthHelp");
-const radio = document.querySelectorAll(".checkbox-input");
+const closeModal = document.querySelectorAll(".close");
+const radio = document.getElementsByName('location');
 const radioHelp = document.querySelector(".radioHelp");
-const quantity = document.querySelector("#quantity");
-const quantityHelp = document.querySelector(".quantityHelp");
+const obligatoire = document.querySelector(".obligatoire");
+let label = document.querySelectorAll('label');
+console.log(label);
 let inputField = document.querySelectorAll("input.text-control");
+let allInput = document.querySelectorAll('input');
 let help = document.querySelectorAll(".help");
+
+console.log(radio);
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
-function launchModal() {
+function launchModal() {;
   modalbg.style.display = "block";
   focusBlur();
   closeForm();
 }
 
-//  Close modal formulaire avec la croix
-function closeForm() {
-  closeModal.addEventListener("click", () => {
-    closeModal.style.display = "none";
-    setTimeout(() => {
-      location.reload();
-    }, 200);
-  });
+// Fonction RESET après envoi du formulaire
+function resetField (){
+  for(let i = 0; i < inputField.length; i++){
+    inputField[i].value = "";
+    inputField[i].classList.remove('redBorder');
+    inputField[i].classList.remove('greenBorder');
+    inputField[i].nextElementSibling.innerHTML = "";
+    
+  }
+  radioHelp.innerHTML = `Vous devez faire un choix`;
+  radioHelp.classList.remove('green');
+  radioHelp.classList.add('red');
+  obligatoire.innerHTML = `obligatoire`;
+  obligatoire.classList.remove('green');
+  obligatoire.classList.add('red');
+  for( let oneRadio of radio){
+      oneRadio.checked = false;
+  }
+  checkbox1.checked = false;
+  checkbox2.checked = false;
 }
 
-// Focus et Blur des 5 premiers input
-function focusBlur() {
-  for (let i = 0; i < inputField.length; i++) {
-    inputField[i].addEventListener("focus", addColor);
-    inputField[i].addEventListener("blur", removeColor);
 
-    function addColor() {
-      inputField[i].style.border = "3px solid blue";
+ //Close modal formulaire avec la croix
+ function closeForm() {
+  closeModal.forEach((btn) => btn.addEventListener("click",  () => {
+        modalbg.style.display = 'none';
+        modalbg2.style.display = 'none';
+        setTimeout(() => {
+          resetField()
+        }, 1000);  
+  }))
+ }
+
+ //Close modal formulaire avec le bouton
+ function closeFormButton(){
+    btnCloseModal.addEventListener('click', () => {
+      modalbg.style.display = 'none';
+        modalbg2.style.display = 'none';
+        setTimeout(() => {
+          resetField();
+        }, 1000);  
+    })
+ }
+
+function focusBlur(){
+  for(let i = 0; i < inputField.length; i++){
+    inputField[i].addEventListener('focus', addColor);
+    inputField[i].addEventListener('blur', removeColor);
+
+    function addColor(){
+      inputField[i].classList.add('blueBorder');
+     
     }
 
-    function removeColor() {
-      inputField[i].style.border = "none";
+    function removeColor(){
+      inputField[i].classList.remove('blueBorder');
+     
     }
-  }
+  }  
 }
 
 // fonction changement d'apparence du formumaire après validation
-function formChange() {
-  //close modal après validation
-  const btnSubmit = document.querySelector(".btn-submit");
-  const modalbg2 = document.querySelector(".bground-2");
 
-  btnSubmit.addEventListener("click", () => {
+function formChange() {
     modalbg.style.display = "none";
     modalbg2.style.display = "block";
-  });
-  const btnCloseModal = document.querySelector(".btn-closeModal");
-  btnCloseModal.addEventListener("click", () => {
-    modalbg.style.display = "none";
-    modalbg2.style.display = "none";
-    setTimeout(() => {
-      location.reload();
-    }, 1000);
-  });
 }
 
+// Fonction clic sur label = focus sur input
+function labelFocus(){
+  label.forEach(lab => {
+    lab.addEventListener('click', () => {
+        lab.nextElementSibling.focus();
+    })
+    })
+  }
+labelFocus();
 
-function messageOK(nodeElt, message) {
-  nodeElt.innerHTML = message;
-  nodeElt.style.color = '#70e000';
-}
-function error(nodeElt, message){
-  nodeElt.innerHTML = message;
-  nodeElt.style.color = "#fe142f";
-  
-}
 
-// Validations du prenom
+// Fonction validation des 5 premiers champs
 
-  function validePrenom() {
-    if (prenom.value === "") {
-      error(prenomHelp, `Veuillez renseigner votre prénom`)
-      return false;
-    } else if (/^[a-zA-Zéè ]+$/.test(prenom.value.trim()) && prenom.value.length >= 2) {
-      messageOK(prenomHelp, `Prénom valide`)
-      nom.focus();
-      prenom.style.border = "2px solid #16d12f";
-      return true;
-    } else {
-      error(prenomHelp, `Le prénom doit contenir 2 lettres minmum`)
-      return false;
+function valideForm() {
+  let error = 0;
+  for (let i = 0; i < inputField.length; i++) {
+    const type = inputField[i].getAttribute("id");
+    let paragraphe = inputField[i].nextElementSibling;
+
+    switch (type) {
+      case "first":
+        if (inputField[i].value === "" || inputField[i].value === null) {
+          error = error + 1;
+          inputField[i].classList.remove('blueBorder');
+          okMessageRemove(paragraphe, inputField[i]);
+          errorMessage(paragraphe, `Veuillez inscrire votre prénom`, inputField[i]);
+        } else if (
+          /^[a-zA-Zéè ]+$/.test(inputField[i].value.trim()) &&
+          inputField[i].value.length >= 2
+        ) {
+          inputField[i].classList.remove('blueBorder');
+          errorMessageRemove(paragraphe, inputField[i]);
+          okMessage(paragraphe, `Le prénom est valide`, inputField[i]);
+          inputField[i + 1].focus();
+        }else{
+          error = error + 1;
+          inputField[i].classList.remove('blueBorder');
+          okMessageRemove(paragraphe, inputField[i]);
+          errorMessage(paragraphe, `Le prénom est invalide`, inputField[i]);
+        }
+        
+        break;
+
+      case "last":
+
+        if (inputField[i].value === "" || inputField[i].value === null) {
+          okMessageRemove(paragraphe, inputField[i]);
+          errorMessage(paragraphe, `Veuillez renseigner votre nom`, inputField[i]);
+          error = error + 1;
+        } else if (
+          /^[a-zA-Zéè ]+$/.test(inputField[i].value.trim()) &&
+          inputField[i].value.length >= 2
+        ) {
+          inputField[i].classList.remove('blueBorder');
+          errorMessageRemove(paragraphe, inputField[i])
+          okMessage(paragraphe, `Nom valide`, inputField[i]);
+          inputField[i + 1].focus();
+        }else{
+          inputField[i].classList.remove('blueBorder');
+          okMessageRemove(paragraphe, inputField[i]);
+          errorMessage(paragraphe, `Nom invalide`, inputField[i]);
+          error = error + 1;
+        }
+
+        break;
+
+      case "email":
+
+        if (inputField[i].value === "" || inputField[i].value === null) {
+          okMessageRemove(paragraphe, inputField[i]);
+          errorMessage(paragraphe, `Le mail est obligatoire`, inputField[i]);
+          error = error + 1;
+        } else if (
+          /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(
+            inputField[i].value.trim())){
+              inputField[i].classList.remove('blueBorder');
+              errorMessageRemove(paragraphe, inputField[i]);
+              okMessage(paragraphe, `Mail valide`, inputField[i]);
+              inputField[i + 1].focus();
+        }else{
+          inputField[i].classList.remove('blueBorder');
+          okMessageRemove(paragraphe, inputField[i]);
+          errorMessage(paragraphe, `Le mail est invalide`, inputField[i]);
+          error = error + 1;
+        }
+        
+        break;
+
+      case "birthdate":
+        const birthday = new Date(inputField[i].value);
+        const todayTime = new Date(); //.toISOString().split('T')[0];
+        const dateMin = new Date(1920, 0, 1);
+        const ageMax = new Date(todayTime - 315576e5 * 12); //.toISOString().split('T')[0] // 12 ans depuis aujourd'hui
+        okMessageRemove(paragraphe, inputField[i]);
+        errorMessage(paragraphe, `Le champ est obligatoire`, inputField[i]);
+        if (birthday > todayTime || birthday >= ageMax || birthday <= dateMin) {
+          inputField[i].classList.remove('blueBorder');
+          okMessageRemove(paragraphe, inputField[i]);
+          errorMessage(paragraphe, `Le champ est invalide`, inputField[i]);
+          error = error + 1;
+        } else if (
+          birthday < todayTime &&
+          birthday <= ageMax &&
+          birthday >= dateMin
+        ) {
+          inputField[i].classList.remove('blueBorder');
+          errorMessageRemove(paragraphe, inputField[i]);
+          okMessage(paragraphe, `Date de naissance valide`, inputField[i]);
+          inputField[i + 1].focus();
+        }
+
+        break;
+
+      case "quantity":
+        if (inputField[i].value === "" || inputField[i].value === null) {
+          inputField[i].classList.remove('blueBorder');
+          okMessageRemove(paragraphe, inputField[i]);
+          errorMessage(paragraphe, `Le champ est invalide`, inputField[i]);
+          error = error + 1;
+        } else {
+          inputField[i].classList.remove('blueBorder');
+          errorMessageRemove(paragraphe, inputField[i]);
+          okMessage(paragraphe, `Champ valide`, inputField[i])
+        }
+
+        break;
     }
   }
+  return error <= 0;
   
-
-  // Validation du nom
-
-function valideNom() {
-  if (nom.value === "") {
-    error(nomHelp, `Veuillez renseigner votre nom`)
-    return false;
-  } else if (/^[a-zA-Zéè ]+$/.test(nom.value.trim()) && nom.value.length >= 2) {
-    messageOK(nomHelp, `Nom valide`)
-    mail.focus();
-    nom.style.border = "2px solid #16d12f";
-    return true;
-  } else {
-    error(nomHelp, `Le nom ne doit contenir que des lettres et avoir 2 caractères minimum`)
-    return false;
-  }
 }
 
-
-// Validation du mail
-
-function valideMail() {
-  if (mail.value === "") {
-    error(mailHelp, `Le mail est obligatoire`)
-    return false;
-  } else if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail.value.trim())) {
-    messageOK(mailHelp, `Mail valide`)
-    birth.focus();
-    mail.style.border = "2px solid #16d12f";
-    return true;
-  } else {
-    error(mailHelp, `Mail invalide`)
-    return false;
-  }
-}
-
-
-// Validation de la date de naissance
-
-function valideBirth() {
-  const birthday = new Date(birth.value);
-  const todayTime = new Date();
-  const dateMin = new Date(todayTime - 31536e8); // 100 ans
-  const ageMax = new Date(todayTime - 378432e6); // 12 ans
-
-  error(birthHelp, `Date de naissance invalide`)
-  if (birthday > todayTime || birthday >= ageMax || birthday <= dateMin) {
-    error(birthHelp, `Date de naissance invalide`)
-    return false;
-  } else if (
-    birthday < todayTime &&
-    birthday <= ageMax &&
-    birthday >= dateMin
-  ) {
-    messageOK(birthHelp, "Date de naissance valide");
-    quantity.focus();
-    birth.style.border = "2px solid #16d12f";
-    return true;
-  }
-}
-
-
-function valideQuantity() {
-  if (quantity.value === "") {
-    error(quantityHelp, `Champ obligatoire`);
-  } else {
-    messageOK(quantityHelp, "Champ valide");
-    quantity.style.border = "2px solid #16d12f";
-    return true;
-  }
-}
-
-
-function valideRadio() {
-  let radioChecked = 0;
-  error(radioHelp, `Champ obligatoire`);
-  for (let i = 0; i < radio.length; i++) {
-    if (radio[i].checked) {
-      radioChecked++;
-      if (radioChecked) {
-        messageOK(radioHelp, `Vous avez choisi ${radio[i].value}`);
-        return true;
-      } else {
-        error(radioHelp, `Champ obligatoire`);
-        return false;
+function valideRadio() {  
+  radioHelp.innerHTML = `Vous devez faire un choix`;
+  radioHelp.classList.add('red');
+    for(let rad of radio) {
+      if(rad.checked){
+        radioHelp.innerHTML = `Vous avez choisi ${rad.value}`;
+        radioHelp.classList.remove('red');
+        radioHelp.classList.add('green'); 
+        return true;  
       }
     }
-  }   
-}
-  
-valideRadio()
+  }
+ 
+valideRadio();
 
 function valideConditions() {
   const checkbox1 = document.querySelector("#checkbox1");
-  const obligatoire = document.querySelector(".obligatoire");
-  error(obligatoire, `Obligatoire`);
+  obligatoire.innerHTML = `obligatoire`;
+  obligatoire.classList.add('red');
   if (checkbox1.checked === true) {
-    messageOK(obligatoire, `Merci !`)
+    obligatoire.innerHTML = "Merci !";
+    obligatoire.classList.add('green');
     return true;
-  } else if (checkbox1 === false) {
-    error(obligatoire, `Obligatoire`);
+  } else{
+    obligatoire.innerHTML = `obligatoire`;
+    obligatoire.classList.add('red');
     return false;
   }
 }
+
 valideConditions();
 
+function errorMessage(nodeElt, message, input) {
+  nodeElt.innerHTML = message;
+  nodeElt.classList.add('red');
+  input.classList.add('redBorder')
+}
 
-//Validation du formulaire
+function okMessage(nodeElt, message, input){
+  nodeElt.innerHTML = message;
+  nodeElt.classList.add('green');
+  input.classList.add('greenBorder')
+}
+
+function okMessageRemove(nodeElt, input){
+  nodeElt.classList.remove('green');
+  input.classList.remove('greenBorder')
+}
+
+function errorMessageRemove(nodeElt, input){
+  nodeElt.classList.remove('green');
+  input.classList.remove('greenBorder');
+  
+}
+
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   if (
-    validePrenom() &&
-    valideNom() &&
-    valideMail() &&
-    valideBirth() &&
-    valideQuantity() &&
+    valideForm()&&
     valideRadio() &&
     valideConditions()
   ) {
     setTimeout(() => {
       formChange();
     }, 1000);
-  }else{
-    for(let input of inputField){
-      if(!input.value){
-        input.style.border = "2px solid #fe142f";
-      } 
-    } 
+    closeForm();
+    closeFormButton();
   }
-
-  closeForm();
-});
-
+})
